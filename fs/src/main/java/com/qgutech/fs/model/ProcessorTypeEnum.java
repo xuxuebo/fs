8dc9445acf7177efa2fbc0f0137eca3e6cca866d
zhaojie <zhaojie@21tb.com>
2016-10-31 14:10:30 +0800

    完善ProcessorTypeEnum类型

--- fs/src/main/java/com/qgutech/fs/model/ProcessorTypeEnum.java
+++ fs/src/main/java/com/qgutech/fs/model/ProcessorTypeEnum.java
@@ -1,10 +1,14 @@
 package com.qgutech.fs.model;
 
 public enum ProcessorTypeEnum {
-    VIDEO,
-    AUDIO,
-    IMAGE,
-    DOC,
-    FILE
-
+    VID,//视频
+    AUD,//音频
+    IMG,//图片
+    DOC,//文档
+    FILE,//普通上传文件，不做处理
+    ZIP,//压缩文件，只做加压处理
+    ZVID,//压缩文件，里面的都是视频，做视频处理
+    ZAUD,//压缩文件，里面的都是音频，做音频处理
+    ZIMG,//压缩文件，里面的都是图片，做图片处理
+    ZDOC//压缩文件，里面的都是文档，做文档处理
 }
--- fs/src/main/java/com/qgutech/fs/model/StoredFile.java
+++ fs/src/main/java/com/qgutech/fs/model/StoredFile.java
@@ -1,15 +1,78 @@
 package com.qgutech.fs.model;
 
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
 public class StoredFile {
 
-    protected String storedFileId;
-    protected String storedFileName;
-    protected String processor;
-    protected String appCode;
-    protected String corpCode;
-    protected Integer subFileCount;
-    protected Long fileSize;
-    protected String businessId;
+    /**
+     * 上传文件在文件系统中的主键
+     */
+    private String storedFileId;
+
+    /**
+     * 上传文件的文件名（源文件名称）
+     */
+    private String storedFileName;
+
+    /**
+     * 处理类型（上传时必须指定）
+     *
+     * @see com.qgutech.fs.model.ProcessorTypeEnum
+     */
+    private String processor;
+
+    /**
+     * 应用编号（上传时必须指定）
+     */
+    private String appCode;
+
+    /**
+     * 公司编号（上传时必须指定）
+     */
+    private String corpCode;
+
+    /**
+     * 子文件数
+     */
+    private Integer subFileCount;
+
+    /**
+     * 文件大小
+     */
+    private Long fileSize;
+
+    /**
+     * 关联的业务主键（上传时必须指定）
+     */
+    private String businessId;
+
+    /**
+     * 关联的业务编号
+     */
+    private String businessCode;
+
+    /**
+     * 指定业务目录，指定时可以将一些指定业务的关联的文件放到该目录下
+     */
+    private String businessDir;
+
+    /**
+     * 文件尾缀
+     */
+    private String suffix;
+
+    /**
+     * 上传的文件对象
+     */
+    private MultipartFile file;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
 
 }

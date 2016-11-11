13100a4b9a74a7622fd6e23a3bbd73b93e937294
zhaojie <zhaojie@21tb.com>
2016-11-11 17:42:53 +0800

    完成ZipProcessor

--- fs/src/main/java/com/qgutech/fs/processor/ZipProcessor.java
+++ fs/src/main/java/com/qgutech/fs/processor/ZipProcessor.java
@@ -1,6 +1,60 @@
 package com.qgutech.fs.processor;
 
 
+import com.qgutech.fs.domain.FsFile;
+import com.qgutech.fs.utils.FsConstants;
+import com.qgutech.fs.utils.FsUtils;
+import com.qgutech.fs.utils.PropertiesUtils;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ZipProcessor extends AbstractProcessor {
 
+    @Override
+    protected boolean validateFile(FsFile fsFile) throws Exception {
+        boolean valid = validateZip(fsFile.getSuffix());
+        if (!valid) {
+            return false;
+        }
+
+        String tmpFilePath = fsFile.getTmpFilePath();
+        File parentFile = new File(tmpFilePath).getParentFile();
+        File decompressDir = new File(parentFile, FsConstants.DECOMPRESS);
+        FsUtils.decompress(tmpFilePath, decompressDir.getAbsolutePath());
+        File indexFile = new File(decompressDir, FsConstants.ZIP_INDEX_FILE);
+        return indexFile.exists();
+    }
+
+    @Override
+    protected boolean needAsync(FsFile fsFile) {
+        return false;
+    }
+
+    @Override
+    protected String getGenFilePath(FsFile fsFile) {
+        return PropertiesUtils.getFileStoreDir() + fsFile.getCorpCode()
+                + File.separator + fsFile.getAppCode()
+                + File.separator + FsConstants.FILE_DIR_GEN
+                + File.separator + FsConstants.FILE_DIR_UNZIP
+                + File.separator + FsUtils.formatDateToYYMM(fsFile.getCreateTime())
+                + File.separator + fsFile.getId();
+    }
+
+    @Override
+    public void process(FsFile fsFile) throws Exception {
+        String genFilePath = getGenFilePath(fsFile);
+        File genFile = new File(genFilePath);
+        if (!genFile.exists() && !genFile.mkdirs()) {
+            throw new IOException("Creating directory[path:" + genFile.getAbsolutePath() + "] failed!");
+        }
+
+        String tmpFilePath = fsFile.getTmpFilePath();
+        File parentFile = new File(tmpFilePath).getParentFile();
+        File decompressDir = new File(parentFile, FsConstants.DECOMPRESS);
+        FileUtils.copyDirectory(decompressDir, genFile);
+        
+        afterProcess(fsFile);
+    }
 }

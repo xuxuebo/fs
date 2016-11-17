c66b0269da4852939adfc9249ed0342491449875
zhaojie <zhaojie@21tb.com>
2016-11-17 16:06:52 +0800

    完成fs调用fs-service的文件表的增删该查

--- fs-service/src/main/java/com/qgutech/fs/controller/FileServerController.java
+++ fs-service/src/main/java/com/qgutech/fs/controller/FileServerController.java
@@ -35,44 +35,18 @@ public class FileServerController {
     public void getFile(FsFile fsFile, HttpServletResponse response) throws Exception {
         PrintWriter writer = response.getWriter();
         try {
-            String sign = fsFile.getSign();
-            String serverHost = fsFile.getServerHost();
-            Long timestamp = fsFile.getTimestamp();
-            String fsFileId = fsFile.getId();
-            String serverCode = fsFile.getServerCode();
-            if (StringUtils.isEmpty(sign) || StringUtils.isEmpty(serverHost)
-                    || timestamp == null || StringUtils.isEmpty(fsFileId)
-                    || StringUtils.isEmpty(serverCode)) {
-                writer.write(FsConstants.RESPONSE_RESULT_ERROR);
+            if (!validate(fsFile, writer, true)) {
                 return;
             }
 
-            long timeMillis = System.currentTimeMillis();
-            if (timeMillis - timestamp >= PropertiesUtils.getMaxWaitForRequest()) {
-                writer.write(FsConstants.RESPONSE_RESULT_ERROR);
-                return;
-            }
-
-            FsServer fsServer = fsServerService.getFsServerByServerHostAndServerCode(serverHost, serverCode);
-            if (fsServer == null) {
-                writer.write(FsConstants.RESPONSE_RESULT_ERROR);
-                return;
-            }
-
-            String newSign = Signer.sign(fsFileId, serverHost, fsServer.getSecret(), timestamp);
-            if (!sign.equals(newSign)) {
-                writer.write(FsConstants.RESPONSE_RESULT_ERROR);
-                return;
-            }
-
-            FsFile file = fsFileService.get(fsFileId);
+            FsFile file = fsFileService.get(fsFile.getId());
             if (file == null) {
                 return;
             }
 
             writer.write(gson.toJson(file));
         } catch (Exception e) {
-            LOG.error("Exception occurred  when save fsFile[" + fsFile + "]!", e);
+            LOG.error("Exception occurred  when getting fsFile by condition[" + fsFile + "]!", e);
             writer.write(FsConstants.RESPONSE_RESULT_ERROR);
         } finally {
             IOUtils.closeQuietly(writer);
@@ -83,39 +57,13 @@ public class FileServerController {
     public void deleteFile(FsFile fsFile, HttpServletResponse response) throws Exception {
         PrintWriter writer = response.getWriter();
         try {
-            String sign = fsFile.getSign();
-            String serverHost = fsFile.getServerHost();
-            Long timestamp = fsFile.getTimestamp();
-            String fsFileId = fsFile.getId();
-            String serverCode = fsFile.getServerCode();
-            if (StringUtils.isEmpty(sign) || StringUtils.isEmpty(serverHost)
-                    || timestamp == null || StringUtils.isEmpty(fsFileId)
-                    || StringUtils.isEmpty(serverCode)) {
-                writer.write(FsConstants.RESPONSE_RESULT_ERROR);
+            if (!validate(fsFile, writer, true)) {
                 return;
             }
 
-            long timeMillis = System.currentTimeMillis();
-            if (timeMillis - timestamp >= PropertiesUtils.getMaxWaitForRequest()) {
-                writer.write(FsConstants.RESPONSE_RESULT_ERROR);
-                return;
-            }
-
-            FsServer fsServer = fsServerService.getFsServerByServerHostAndServerCode(serverHost, serverCode);
-            if (fsServer == null) {
-                writer.write(FsConstants.RESPONSE_RESULT_ERROR);
-                return;
-            }
-
-            String newSign = Signer.sign(fsFileId, serverHost, fsServer.getSecret(), timestamp);
-            if (!sign.equals(newSign)) {
-                writer.write(FsConstants.RESPONSE_RESULT_ERROR);
-                return;
-            }
-
-            fsFileService.delete(fsFileId);
+            fsFileService.delete(fsFile.getId());
         } catch (Exception e) {
-            LOG.error("Exception occurred  when save fsFile[" + fsFile + "]!", e);
+            LOG.error("Exception occurred  when deleting fsFile by condition[" + fsFile + "]!", e);
             writer.write(FsConstants.RESPONSE_RESULT_ERROR);
         } finally {
             IOUtils.closeQuietly(writer);
@@ -126,38 +74,14 @@ public class FileServerController {
     public void saveFile(FsFile fsFile, HttpServletResponse response) throws Exception {
         PrintWriter writer = response.getWriter();
         try {
-            String sign = fsFile.getSign();
-            String serverHost = fsFile.getServerHost();
-            Long timestamp = fsFile.getTimestamp();
-            String serverCode = fsFile.getServerCode();
-            if (StringUtils.isEmpty(sign) || StringUtils.isEmpty(serverHost)
-                    || timestamp == null || StringUtils.isEmpty(serverCode)) {
-                writer.write(FsConstants.RESPONSE_RESULT_ERROR);
-                return;
-            }
-
-            long timeMillis = System.currentTimeMillis();
-            if (timeMillis - timestamp >= PropertiesUtils.getMaxWaitForRequest()) {
-                writer.write(FsConstants.RESPONSE_RESULT_ERROR);
-                return;
-            }
-
-            FsServer fsServer = fsServerService.getFsServerByServerHostAndServerCode(serverHost, serverCode);
-            if (fsServer == null) {
-                writer.write(FsConstants.RESPONSE_RESULT_ERROR);
-                return;
-            }
-
-            String newSign = Signer.sign(serverHost, fsServer.getSecret(), timestamp);
-            if (!sign.equals(newSign)) {
-                writer.write(FsConstants.RESPONSE_RESULT_ERROR);
+            if (!validate(fsFile, writer, false)) {
                 return;
             }
 
             String fsFileId = fsFileService.save(fsFile);
             writer.write(fsFileId);
         } catch (Exception e) {
-            LOG.error("Exception occurred  when save fsFile[" + fsFile + "]!", e);
+            LOG.error("Exception occurred when saving fsFile[" + fsFile + "]!", e);
             writer.write(FsConstants.RESPONSE_RESULT_ERROR);
         } finally {
             IOUtils.closeQuietly(writer);
@@ -168,50 +92,75 @@ public class FileServerController {
     public void updateFile(FsFile fsFile, HttpServletResponse response) throws Exception {
         PrintWriter writer = response.getWriter();
         try {
-            String fsFileId = fsFile.getId();
-            String sign = fsFile.getSign();
-            String serverHost = fsFile.getServerHost();
-            Long timestamp = fsFile.getTimestamp();
-            String serverCode = fsFile.getServerCode();
-            if (StringUtils.isEmpty(fsFileId) || StringUtils.isEmpty(sign)
-                    || StringUtils.isEmpty(serverHost) || timestamp == null
-                    || StringUtils.isEmpty(serverCode)) {
-                writer.print(FsConstants.RESPONSE_RESULT_ERROR);
+            if (!validate(fsFile, writer, true)) {
                 return;
             }
 
+            String fsFileId = fsFile.getId();
             FsFile dbFsFile = fsFileService.get(fsFileId);
             if (dbFsFile == null) {
-                writer.print(FsConstants.RESPONSE_RESULT_ERROR);
-                return;
-            }
-
-            long timeMillis = System.currentTimeMillis();
-            if (timeMillis - timestamp >= PropertiesUtils.getMaxWaitForRequest()) {
+                LOG.error("FsFile[id:" + fsFileId + "] not exist when updating fsFile!");
                 writer.print(FsConstants.RESPONSE_RESULT_ERROR);
                 return;
             }
 
             dbFsFile.merge(fsFile);
-            FsServer fsServer = fsServerService.getFsServerByServerHostAndServerCode(serverHost, serverCode);
-            if (fsServer == null) {
-                writer.print(FsConstants.RESPONSE_RESULT_ERROR);
-                return;
-            }
-
-            String newSign = Signer.sign(fsFileId, serverHost, fsServer.getSecret(), timestamp);
-            if (!sign.equals(newSign)) {
-                writer.print(FsConstants.RESPONSE_RESULT_ERROR);
-                return;
-            }
-
             fsFileService.update(dbFsFile);
             writer.print(FsConstants.RESPONSE_RESULT_SUCCESS);
         } catch (Exception e) {
-            LOG.error("Exception occurred  when update fsFile[" + fsFile + "]!", e);
+            LOG.error("Exception occurred  when updating fsFile[" + fsFile + "]!", e);
             writer.print(FsConstants.RESPONSE_RESULT_ERROR);
         } finally {
             IOUtils.closeQuietly(writer);
         }
+    }
+
+    private boolean validate(FsFile fsFile, PrintWriter writer, boolean checkFsFileId) {
+        String sign = fsFile.getSign();
+        String serverHost = fsFile.getServerHost();
+        Long timestamp = fsFile.getTimestamp();
+        String fsFileId = fsFile.getId();
+        String serverCode = fsFile.getServerCode();
+        if (StringUtils.isEmpty(sign) || StringUtils.isEmpty(serverHost)
+                || timestamp == null || (checkFsFileId && StringUtils.isEmpty(fsFileId))
+                || StringUtils.isEmpty(serverCode)) {
+            LOG.error("One of the param[sign:" + sign + ",serverHost:" + serverHost + ",timestamp:"
+                    + timestamp + (checkFsFileId ? (",id:" + fsFileId) : StringUtils.EMPTY)
+                    + ",serverCode:" + serverCode + "] is null or empty!");
+            writer.write(FsConstants.RESPONSE_RESULT_ERROR);
+            return false;
+        }
+
+        long timeMillis = System.currentTimeMillis();
+        long maxWaitForRequest = PropertiesUtils.getMaxWaitForRequest();
+        if (timeMillis - timestamp >= maxWaitForRequest) {
+            LOG.error("The request timeout because of  the current timestamp[" + timeMillis
+                    + "] subtract param timestamp[" + timestamp
+                    + "] bigger than maxWaitTime[" + maxWaitForRequest + "]!");
+            writer.write(FsConstants.RESPONSE_RESULT_ERROR);
+            return false;
+        }
+
+        FsServer fsServer = fsServerService.getFsServerByServerHostAndServerCode(serverHost, serverCode);
+        if (fsServer == null) {
+            LOG.error("Can not find a fs server which  host is "
+                    + serverHost + " and serverCode is " + serverCode + " !");
+            writer.write(FsConstants.RESPONSE_RESULT_ERROR);
+            return false;
+        }
+
+        String newSign = checkFsFileId ? Signer.sign(fsFileId, serverHost, fsServer.getSecret(), timestamp)
+                : Signer.sign(serverHost, fsServer.getSecret(), timestamp);
+        if (!sign.equals(newSign)) {
+            LOG.error("The generating sign[sign:" + newSign
+                    + (checkFsFileId ? (",id:" + fsFileId) : StringUtils.EMPTY)
+                    + ",serverHost:" + serverHost + ",timestamp:" + timestamp
+                    + ",secret:" + fsServer.getSecret()
+                    + "] is not equal the param sign[sign:" + sign + "]!");
+            writer.write(FsConstants.RESPONSE_RESULT_ERROR);
+            return false;
+        }
+
+        return true;
     }
 }

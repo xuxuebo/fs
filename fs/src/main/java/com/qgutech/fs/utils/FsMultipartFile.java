3f8067db30801df30e4ceed3515c3d570c8a4b7a
zhaojie <zhaojie@21tb.com>
2016-11-30 17:47:27 +0800

    改为cos上传文件

---
+++ fs/src/main/java/com/qgutech/fs/utils/FsMultipartFile.java
@@ -0,0 +1,88 @@
+package com.qgutech.fs.utils;
+
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+
+public class FsMultipartFile implements MultipartFile {
+    private String fileName;
+    private File file;
+    private String originalFileName;
+    private String contentType;
+    private InputStream inputStream;
+
+    public FsMultipartFile(String fileName, File file
+            , String originalFileName, String contentType) {
+        this.fileName = fileName;
+        this.file = file;
+        this.originalFileName = originalFileName;
+        this.contentType = contentType;
+    }
+
+    @Override
+    public String getName() {
+        return this.fileName;
+    }
+
+    @Override
+    public String getOriginalFilename() {
+        return this.originalFileName;
+    }
+
+    @Override
+    public String getContentType() {
+        return this.contentType;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return file.length() == 0;
+    }
+
+    @Override
+    public long getSize() {
+        return file.length();
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        InputStream inputStream = getInputStream();
+        if (inputStream == null) {
+            return new byte[0];
+        }
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        IOUtils.copy(inputStream, byteArrayOutputStream);
+
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        if (inputStream == null) {
+            inputStream = new FileInputStream(file);
+        }
+
+        return inputStream;
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException, IllegalStateException {
+        if (dest.exists() && !dest.delete()) {
+            throw new IOException("Destination file [" + dest.getAbsolutePath()
+                    + "] already exists and could not be deleted!");
+        }
+
+        FileUtils.copyFile(file, dest);
+    }
+
+    public void cleanup() {
+        IOUtils.closeQuietly(inputStream);
+        //todo 是否需要删除文件
+        file.delete();
+    }
+}
---
+++ fs/src/main/java/com/qgutech/fs/utils/FsMultipartResolver.java
@@ -0,0 +1,176 @@
+package com.qgutech.fs.utils;
+
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+import org.springframework.util.Assert;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.DefaultMultipartHttpServletRequest;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FsMultipartResolver implements MultipartResolver {
+
+    private boolean resolveLazily = false;
+    private String saveDirectory;
+    private int maxPostSize = 1024 * 1024 * 1024;
+
+    @Override
+    public boolean isMultipart(HttpServletRequest request) {
+        if (request == null) {
+            return false;
+        }
+
+        if (!"post".equals(request.getMethod().toLowerCase())) {
+            return false;
+        }
+
+        String contentType = request.getContentType();
+        return contentType != null && contentType.toLowerCase().startsWith("multipart/");
+    }
+
+    @Override
+    public MultipartHttpServletRequest resolveMultipart(final HttpServletRequest request)
+            throws MultipartException {
+        Assert.notNull(request, "Request must not be null");
+        if (this.resolveLazily) {
+            return new DefaultMultipartHttpServletRequest(request) {
+                @Override
+                protected void initializeMultipart() {
+                    MultipartParsingResult parsingResult = parseRequest(request);
+                    setMultipartFiles(parsingResult.getMultipartFiles());
+                    setMultipartParameters(parsingResult.getMultipartParameters());
+                    setMultipartParameterContentTypes(parsingResult.getMultipartParameterContentTypes());
+                }
+            };
+        } else {
+            MultipartParsingResult parsingResult = parseRequest(request);
+            return new DefaultMultipartHttpServletRequest(request, parsingResult.getMultipartFiles(),
+                    parsingResult.getMultipartParameters(), parsingResult.getMultipartParameterContentTypes());
+        }
+    }
+
+    protected MultipartParsingResult parseRequest(HttpServletRequest request) {
+        try {
+            MultipartRequest multipartRequest =
+                    new MultipartRequest(request, saveDirectory, maxPostSize
+                            , determineEncoding(request), new DefaultFileRenamePolicy());
+            MultiValueMap<String, MultipartFile> multipartFiles =
+                    new LinkedMultiValueMap<String, MultipartFile>();
+            Map<String, String[]> multipartParameters = new HashMap<String, String[]>();
+            Map<String, String> multipartParameterContentTypes = new HashMap<String, String>();
+            Enumeration fileNames = multipartRequest.getFileNames();
+            while (fileNames.hasMoreElements()) {
+                String fileName = (String) fileNames.nextElement();
+                File file = multipartRequest.getFile(fileName);
+                String originalFileName = multipartRequest.getOriginalFileName(fileName);
+                String contentType = multipartRequest.getContentType(fileName);
+                FsMultipartFile multipartFile = new FsMultipartFile(fileName
+                        , file, originalFileName, contentType);
+                multipartFiles.add(fileName, multipartFile);
+            }
+
+            Enumeration parameterNames = multipartRequest.getParameterNames();
+            while (parameterNames.hasMoreElements()) {
+                String parameterName = (String) parameterNames.nextElement();
+                String contentType = multipartRequest.getContentType(parameterName);
+                multipartParameterContentTypes.put(parameterName, contentType);
+                String[] parameterValues = multipartRequest.getParameterValues(parameterName);
+                multipartParameters.put(parameterName, parameterValues);
+            }
+
+            return new MultipartParsingResult(multipartFiles, multipartParameters
+                    , multipartParameterContentTypes);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected static class MultipartParsingResult {
+
+        private final MultiValueMap<String, MultipartFile> multipartFiles;
+
+        private final Map<String, String[]> multipartParameters;
+
+        private final Map<String, String> multipartParameterContentTypes;
+
+        public MultipartParsingResult(MultiValueMap<String, MultipartFile> mpFiles,
+                                      Map<String, String[]> mpParams, Map<String, String> mpParamContentTypes) {
+            this.multipartFiles = mpFiles;
+            this.multipartParameters = mpParams;
+            this.multipartParameterContentTypes = mpParamContentTypes;
+        }
+
+        public MultiValueMap<String, MultipartFile> getMultipartFiles() {
+            return this.multipartFiles;
+        }
+
+        public Map<String, String[]> getMultipartParameters() {
+            return this.multipartParameters;
+        }
+
+        public Map<String, String> getMultipartParameterContentTypes() {
+            return this.multipartParameterContentTypes;
+        }
+    }
+
+    protected String determineEncoding(HttpServletRequest request) {
+        String encoding = request.getCharacterEncoding();
+        if (encoding == null) {
+            encoding = WebUtils.DEFAULT_CHARACTER_ENCODING;
+        }
+
+        return encoding;
+    }
+
+    @Override
+    public void cleanupMultipart(MultipartHttpServletRequest request) {
+        if (request == null) {
+            return;
+        }
+
+        for (List<MultipartFile> files : request.getMultiFileMap().values()) {
+            for (MultipartFile file : files) {
+                if (file instanceof FsMultipartFile) {
+                    FsMultipartFile fsMultipartFile = (FsMultipartFile) file;
+                    fsMultipartFile.cleanup();
+                }
+            }
+        }
+    }
+
+    public boolean isResolveLazily() {
+        return resolveLazily;
+    }
+
+    public void setResolveLazily(boolean resolveLazily) {
+        this.resolveLazily = resolveLazily;
+    }
+
+    public int getMaxPostSize() {
+        return maxPostSize;
+    }
+
+    public void setMaxPostSize(int maxPostSize) {
+        this.maxPostSize = maxPostSize;
+    }
+
+    public String getSaveDirectory() {
+        return saveDirectory;
+    }
+
+    public void setSaveDirectory(String saveDirectory) {
+        this.saveDirectory = saveDirectory;
+    }
+}

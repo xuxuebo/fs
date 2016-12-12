fa1090e911b0c58f260d57eadd3e2aa1dbe4286d
zhaojie <zhaojie@21tb.com>
2016-12-12 14:33:28 +0800

    修改windowsConvert的实现

--- fs/src/main/java/com/qgutech/fs/convert/Doc2PdfConverter.java
+++ fs/src/main/java/com/qgutech/fs/convert/Doc2PdfConverter.java
@@ -19,4 +19,22 @@ public class Doc2PdfConverter extends AbstractConverter {
         return new File(targetFilePath, FsConstants.PDF_PREFIX
                 + FsUtils.generateUUID() + FsConstants.PDF_SUFFIX);
     }
+
+    @Override
+    protected File windowsConvert(String inputFilePath, String targetFileDirPath) throws Exception {
+        File targetFile = getTargetFile(targetFileDirPath);
+        String[] commands = getCommands(inputFilePath, targetFile.getAbsolutePath());
+        String result = FsUtils.executeCommand(commands);
+        if (result.length() > 0) {
+            if (targetFile.exists()) {
+                LOG.info("Executing command[" + FsUtils.toString(commands)
+                        + "] and result message is:" + result);
+            } else {
+                throw new RuntimeException("Executing command[" + FsUtils.toString(commands)
+                        + "] failed and result message is:" + result);
+            }
+        }
+
+        return targetFile;
+    }
 }

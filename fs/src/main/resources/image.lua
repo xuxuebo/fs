95f3e8b398bda3ad95591b1c87d65da4ab378ce5
zhaojie <zhaojie@21tb.com>
2017-02-16 17:09:36 +0800

    图片的x（y）轴坐标+w（h）不得超过原图的width（height）

--- fs/src/main/resources/image.lua
+++ fs/src/main/resources/image.lua
@@ -2,10 +2,19 @@ local imagePath = ngx.var.imagePath;
 local ffmpeg = ngx.var.ffmpeg;
 local directoryPath = string.match(imagePath, "^(.+)/%d+_%d+_%d+_%d+%.png$")
 if directoryPath ~= nil then
-    local x = string.match(imagePath, "^.+/(%d+)_%d+_%d+_%d+%.png$")
-    local y = string.match(imagePath, "^.+/%d+_(%d+)_%d+_%d+%.png$")
-    local w = string.match(imagePath, "^.+/%d+_%d+_(%d+)_%d+%.png$")
-    local h = string.match(imagePath, "^.+/%d+_%d+_%d+_(%d+)%.png$")
+    local x = tonumber(string.match(imagePath, "^.+/(%d+)_%d+_%d+_%d+%.png$"))
+    local y = tonumber(string.match(imagePath, "^.+/%d+_(%d+)_%d+_%d+%.png$"))
+    local w = tonumber(string.match(imagePath, "^.+/%d+_%d+_(%d+)_%d+%.png$"))
+    local h = tonumber(string.match(imagePath, "^.+/%d+_%d+_%d+_(%d+)%.png$"))
+    local mr = io.popen(ffmpeg .. ' -i ' .. directoryPath .. '/o.png 2>&1')
+    local result = mr:read("*all")
+    local width = tonumber(string.match(result, "Stream.*[^%d]+(%d+)x%d+[^%d]+"))
+    local height = tonumber(string.match(result, "Stream.*[^%d]+%d+x(%d+)[^%d]+"))
+    if (x + w) > width or (y + h) > height then
+        --截图宽度超出图片的总宽度或者截图高度超出图片的总高度
+        ngx.exit(ngx.HTTP_FORBIDDEN)
+    end
+
     local exec = ffmpeg .. ' -i ' .. directoryPath .. '/o.png -vf crop=' .. w .. ':' .. h .. ':' .. x .. ':' .. y .. ' -y ' .. imagePath
     os.execute(exec)
     ngx.exit(ngx.OK)
--- fs/src/main/resources/originImage.lua
+++ fs/src/main/resources/originImage.lua
@@ -2,10 +2,19 @@ local imagePath = ngx.var.imagePath;
 local ffmpeg = ngx.var.ffmpeg;
 local originImage = string.match(imagePath, "^(.+/%w+%.%w+)_%d+_%d+_%d+_%d+%.%w+$")
 if originImage ~= nil then
-    local x = string.match(imagePath, "^.+/%w+%.%w+_(%d+)_%d+_%d+_%d+%.%w+$")
-    local y = string.match(imagePath, "^.+/%w+%.%w+_%d+_(%d+)_%d+_%d+%.%w+$")
-    local w = string.match(imagePath, "^.+/%w+%.%w+_%d+_%d+_(%d+)_%d+%.%w+$")
-    local h = string.match(imagePath, "^.+/%w+%.%w+_%d+_%d+_%d+_(%d+)%.%w+$")
+    local x = tonumber(string.match(imagePath, "^.+/%w+%.%w+_(%d+)_%d+_%d+_%d+%.%w+$"))
+    local y = tonumber(string.match(imagePath, "^.+/%w+%.%w+_%d+_(%d+)_%d+_%d+%.%w+$"))
+    local w = tonumber(string.match(imagePath, "^.+/%w+%.%w+_%d+_%d+_(%d+)_%d+%.%w+$"))
+    local h = tonumber(string.match(imagePath, "^.+/%w+%.%w+_%d+_%d+_%d+_(%d+)%.%w+$"))
+    local mr = io.popen(ffmpeg .. ' -i ' .. originImage .. ' 2>&1')
+    local result = mr:read("*all")
+    local width = tonumber(string.match(result, "Stream.*[^%d]+(%d+)x%d+[^%d]+"))
+    local height = tonumber(string.match(result, "Stream.*[^%d]+%d+x(%d+)[^%d]+"))
+    if (x + w) > width or (y + h) > height then
+        --截图宽度超出图片的总宽度或者截图高度超出图片的总高度
+        ngx.exit(ngx.HTTP_FORBIDDEN)
+    end
+
     local exec = ffmpeg .. ' -i ' .. originImage .. ' -vf crop=' .. w .. ':' .. h .. ':' .. x .. ':' .. y .. ' -y ' .. imagePath
     os.execute(exec)
     ngx.exit(ngx.OK)
